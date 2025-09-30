265e00b765cfe1ae29696c89e4cd95f88be54683
israel.lopez <israel.lopez@106-13.info2>
2025-09-30 13:46:21 +0200

    Exercise 6 and 7

--- src/part2/Exercise06.java
+++ src/part2/Exercise06.java
@@ -13,7 +13,7 @@ public class Exercise06 {
 		double mLongitud;
 		// Declaration variable for result
 		double total;
-		//Declaration variable for conversion
+		// Declaration variable for conversion
 		double ml_cm;
 		double m_cm;
 		// Ask the user for about measurements
@@ -23,15 +23,14 @@ public class Exercise06 {
 		cmLongitud = sc.nextDouble();
 		System.out.println("Introduzca la medida en metros:");
 		mLongitud = sc.nextDouble();
-		//Convert millimeters to centimeters
+		// Convert millimeters to centimeters
 		ml_cm = (double) mlLongitud / 10;
 		m_cm = (double) mLongitud * 10;
-		//Calculation the total
+		// Calculation the total
 		total = cmLongitud + ml_cm + m_cm;
-		//Print the total
+		// Print the total
 		System.out.println("La suma de las tres longitudes da un total de " + total + " cm.");
-		
+
 		sc.close();
 	}
-
 }
--- src/part2/Exercise07.java
+++ src/part2/Exercise07.java
@@ -1,10 +1,26 @@
 package part2;
 
+import java.util.Scanner;
+
 public class Exercise07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Creation a new scanner
+		Scanner sc = new Scanner(System.in);
+		//Declaration number of tickets
+		int entradas;
+		//Declaration prices  
+		final double precioInfantil = 15.50;
+		final double precioAdulto = 20;
+		//Declaration variable if total cost is greater than 100 euro
+		final double mayorDe100 = 0.05;
+		
+		//Ask the user how many tickets want 
+		System.out.println("Cuantos tickets quieres comprar?");
+		entradas = sc.nextInt();
+		
+		
+		sc.close();
 	}
 
 }
