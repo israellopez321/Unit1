481e1e8d2bc66fbcc2b99f74b225058961713304
israel.lopez <israel.lopez@106-13.info2>
2025-10-03 13:19:13 +0200

    Update Exercise 4 // Unit 1

--- src/part2/Exercise04.java
+++ src/part2/Exercise04.java
@@ -5,26 +5,36 @@ import java.util.Scanner;
 public class Exercise04 {
 
 	public static void main(String[] args) {
-		//Creation a new scanner
+		// Creation a new scanner
 		Scanner sc = new Scanner(System.in);
-		//Declaration three numbers (A,B and C)
+		// Declaration three numbers (A,B and C)
 		int numeroA;
 		int numeroB;
 		int numeroC;
-		//Declaration two numbers (X and Y)
+		// Declaration two numbers (X and Y)
 		double numeroX;
 		double numeroY;
-		//Ask the user by the values ​​A, B, C and X
-		System.out.println("Introduzca los valores A, B, C y X para calcular una ecuación de 2º grado:");
+		// Ask the user by the values A
+		System.out.println("Introduzca los valores A para calcular una ecuación de 2º grado:");
+		// Enter values of A
 		numeroA = sc.nextInt();
+		// Ask the user by the values B
+		System.out.println("Introduzca los valores B para calcular una ecuación de 2º grado:");
+		// Enter values of B
 		numeroB = sc.nextInt();
+		// Ask the user by the values ​​C
+		System.out.println("Introduzca los valores C para calcular una ecuación de 2º grado:");
+		// Enter values of C
 		numeroC = sc.nextInt();
+		// Ask the user by the values ​​X
+		System.out.println("Introduzca los valores X para calcular una ecuación de 2º grado:");
+		// Enter values of X
 		numeroX = sc.nextInt();
-		//Calculate the quadratic
+		// Calculate the quadratic
 		numeroY = numeroA * Math.pow(numeroX, 2) + numeroB * numeroX + numeroC;
-		//Print result of quadratic
+		// Print result of quadratic
 		System.out.println("El valor de Y es igual a " + numeroY);
-		
+
 		sc.close();
 	}
 }
